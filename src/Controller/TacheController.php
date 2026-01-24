ffb7c86dcb2e465fa5aab712ca2c7978ffab65d1
Vivien60 <phedras.ch@gmail.com>
2026-01-24 19:20:00 +0100

    Feat : affichage/édition de tâche affichage/édition

--- src/Controller/TacheController.php
+++ src/Controller/TacheController.php
@@ -3,7 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\Tache;
+use App\Form\TacheType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -18,11 +21,20 @@ final class TacheController extends AbstractController
         ]);
     }
 
-    #[Route('/{id}', name: 'app_tache_show', methods: ['GET'])]
-    public function show(Tache $tache): Response
+    #[Route('/{id}', name: 'app_tache_show', methods: ['GET', 'POST'])]
+    public function show(Request $request, Tache $tache, EntityManagerInterface $manager): Response
     {
+        $form = $this->createForm(TacheType::class, $tache);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $manager->persist($tache);
+            $manager->flush();
+
+            return $this->redirectToRoute('app_tache_show', ['id' => $tache->getId()], Response::HTTP_SEE_OTHER);
+        }
         return $this->render('tache/show.html.twig', [
             'tache' => $tache,
+            'form' => $form,
         ]);
     }
 }
---
+++ src/Form/TacheType.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\{Employe, Projet, Statut, Tache};
+use Symfony\Component\Form\AbstractType;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class TacheType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nom', TextType::class, [
+                'required' => true,
+            ])
+            ->add('description', TextareaType::class, [
+                'required' => false,
+            ])
+            ->add('dateDeadline', DateType::class, [
+                'required' => false,
+            ])
+            ->add('employe', EntityType::class, [
+                'required' => false,
+                'class' => Employe::class,
+                'choice_label' => function (Employe $employe) {
+                    return $employe->getPrenom().' '.$employe->getNom();
+                },
+            ])
+            ->add('statut', EntityType::class, [
+                'required' => true,
+                'class' => Statut::class,
+                'choice_label' => 'libelle',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Tache::class,
+        ]);
+    }
+}

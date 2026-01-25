fa512857431b8623b5e2829c10babe89e86c7dab
Vivien60 <phedras.ch@gmail.com>
2026-01-25 23:42:33 +0100

    Feat : édition d'employé

--- src/Controller/EmployeController.php
+++ src/Controller/EmployeController.php
@@ -3,8 +3,11 @@
 namespace App\Controller;
 
 use App\Entity\Employe;
+use App\Form\EmployeType;
 use App\Repository\EmployeRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -20,11 +23,19 @@ final class EmployeController extends AbstractController
         ]);
     }
 
-    #[Route('/{id}', name: 'app_employe_show', methods: ['GET'])]
-    public function show(Employe $employe): Response
+    #[Route('/{id}', name: 'app_employe_edit', requirements: ['id' => '\d+'], methods: ['GET', 'POST'])]
+    public function edit(Employe $employe, Request $request, EntityManagerInterface $manager): Response
     {
-        return $this->render('employe/show.html.twig', [
+        $form = $this->createForm(EmployeType::class, $employe);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $manager->persist($employe);
+            $manager->flush();
+            return $this->redirectToRoute('app_employe_index');
+        }
+        return $this->render('employe/edit.html.twig', [
             'employe' => $employe,
+            'form' => $form,
         ]);
     }
 }
---
+++ src/Form/EmployeType.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Employe;
+use App\Entity\Projet;
+use App\Enum\EmployeTypeContrat;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\EnumType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class EmployeType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('nom', TextType::class, [
+                'required' => true,
+            ])
+            ->add('prenom', TextType::class, [
+                'required' => true,
+            ])
+            ->add('adresseEmail', EmailType::class, [
+                'required' => true,
+            ])
+            ->add('dateEntree', DateType::class, [
+                'required' => true,
+            ])
+            ->add('typeContrat', EnumType::class, [
+                'required' => true,
+                'class' => EmployeTypeContrat::class,
+                'choice_label' => 'getLabel',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Employe::class,
+        ]);
+    }
+}
